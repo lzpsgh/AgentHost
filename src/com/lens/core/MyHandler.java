590210fc0e5f9ee18aa5f708c872800d2447db5c
LensAclrtn <lensaclrtn@gmail.com>
2018-07-22 16:54:45 +0800

    * 提高容错性

--- src/com/lens/core/MyHandler.java
+++ src/com/lens/core/MyHandler.java
@@ -2,7 +2,10 @@ package com.lens.core;
 
 import com.lens.util.LogUtil;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.nio.CharBuffer;
 
@@ -20,39 +23,46 @@ public class MyHandler implements Runnable {
 
     @Override
     public void run() {
+        BufferedReader br = null;
+        PrintWriter pw = null;
         try {
             LogUtil.i("建立连接，" + socket.getInetAddress() + ":" + socket.getPort());
-            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter pw = new PrintWriter(socket.getOutputStream(),true);
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            pw = new PrintWriter(socket.getOutputStream(), true);
             CharBuffer cb = CharBuffer.allocate(1024 * 5);
             String req = "";
             String rsp = "";
             int len = br.read(cb);
+            if (len <= 0) {
+                LogUtil.e("接收报文长度小于0");
+                throw new Exception();
+            }
             req = new String(cb.array()).substring(Config.reqLen, len);
             LogUtil.i("请求报文req:" + req);
             //原始方案
             rsp = getXmlRep.getRep(req);
-
             // 将计算出的报文长度前补0，凑成指定长度的报文头长度值，用于在发送响应前拼接到报文正文前。
             String actualRspLen = String.valueOf(rsp.getBytes().length); // "768"
             if (Config.rspLen < actualRspLen.length()) {
                 LogUtil.e("实际报文长度大于 rspLen 参数");
-            } else {
-                String plusRspLen = Config.reservesZeroStr.substring(0, Config.rspLen - actualRspLen.length()); // "000"
-                String totalRsp = plusRspLen + actualRspLen + rsp;
-                LogUtil.i("实际返回响应报文: \r\n" + totalRsp);
-                pw.write(totalRsp);
-                pw.flush();
+                throw new Exception();
             }
-
-            br.close();
-            pw.close();
+            String plusRspLen = Config.reservesZeroStr.substring(0, Config.rspLen - actualRspLen.length()); // "000"
+            String totalRsp = plusRspLen + actualRspLen + rsp;
+            LogUtil.i("实际返回响应报文: \r\n" + totalRsp);
+            pw.write(totalRsp);
+            pw.flush();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
-                if (socket != null)
+                br.close();
+                pw.close();
+                if (socket != null) {
                     socket.close();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
